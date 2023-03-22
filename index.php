4694d7c28585f8c827d7e1dec310d76b5f69a53a
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-22 15:10:47 +0100

    Suppression des tests sur la chambre

--- index.php
+++ index.php
@@ -17,40 +17,4 @@
     $regentParis = new Hotel("Regent", 3, "61 Rue Dauphine", 75006, "Paris");
 
     $chambre1HiltonStrasbourg = new Chambre($hiltonStrasbourg, 1, 2, 120, "non", "DISPONIBLE");
-    echo $chambre1HiltonStrasbourg;
-    echo "<br/>";
-    echo "Hotel : " . $chambre1HiltonStrasbourg->getHotel();
-    echo "<br>";
-    echo "Chambre n°" . $chambre1HiltonStrasbourg->getNumero();
-    echo "<br>";
-    echo "Nombre de lits :" . $chambre1HiltonStrasbourg->getNombresLits();
-    echo "<br>";
-    echo "Prix : " . $chambre1HiltonStrasbourg->getPrix() . " €";
-    echo "<br>";
-    echo "Wifi : " . $chambre1HiltonStrasbourg->getWifi();
-    echo "<br>";
-    echo "Etat : " . $chambre1HiltonStrasbourg->getEtat();
-    echo "<br>";
-
-    $chambre1HiltonStrasbourg->setHotel($regentParis);
-    $chambre1HiltonStrasbourg->setNumero(2);
-    $chambre1HiltonStrasbourg->setNombresLits(3);
-    $chambre1HiltonStrasbourg->setPrix(180);
-    $chambre1HiltonStrasbourg->setWifi("oui");
-    $chambre1HiltonStrasbourg->setEtat("RÉSERVÉE");
-
-    echo "<br>";
-    echo $chambre1HiltonStrasbourg;
-    echo "<br>";
-    echo "Hotel : " . $chambre1HiltonStrasbourg->getHotel();
-    echo "<br>";
-    echo "Chambre n°" . $chambre1HiltonStrasbourg->getNumero();
-    echo "<br>";
-    echo "Nombre de lits :" . $chambre1HiltonStrasbourg->getNombresLits();
-    echo "<br>";
-    echo "Prix : " . $chambre1HiltonStrasbourg->getPrix() . " €";
-    echo "<br>";
-    echo "Wifi : " . $chambre1HiltonStrasbourg->getWifi();
-    echo "<br>";
-    echo "Etat : " . $chambre1HiltonStrasbourg->getEtat();
 ?>
